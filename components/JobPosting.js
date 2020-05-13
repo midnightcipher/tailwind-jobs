3e4b25ab8baa333bfd4b7641b47780db5337a36b
Adam Wathan <adam.wathan@gmail.com>
2020-05-13 08:27:24 -0400

    Test a redirect

--- components/JobPosting.js
+++ components/JobPosting.js
@@ -100,7 +100,7 @@ export default function JobPosting({ children, metadata }) {
                       </div>
                       <div className="py-4">
                         <dt className="sr-only">Salary</dt>
-                        <dl className="flex items-center space-x-2">
+                        <dl className="flex items-start space-x-2">
                           <svg
                             className="h-6 w-6 text-gray-400"
                             fill="none"
@@ -112,7 +112,7 @@ export default function JobPosting({ children, metadata }) {
                           >
                             <path d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                           </svg>
-                          <span className="text-gray-600">$120,000 USD</span>
+                          <div className="text-gray-600">$105,000 – $120,000 USD</div>
                         </dl>
                       </div>
                       <div className="py-4">
@@ -206,7 +206,7 @@ export default function JobPosting({ children, metadata }) {
                           >
                             <path d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
                           </svg>
-                          <span className="text-gray-600">$120,000 USD</span>
+                          <span className="text-gray-600">$105,000 – $120,000 USD</span>
                         </dl>
                       </div>
                       <div className="py-4">
